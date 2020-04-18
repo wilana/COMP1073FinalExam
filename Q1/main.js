867822e9accf6b54fc26e5b1cc2aaf298bdeb4bd
wilana <wsmatthe@lakeheadu.ca>
2020-04-18 14:14:25 -0400

    Q1

--- Q1/main.js
+++ Q1/main.js
@@ -4,19 +4,21 @@
 // only want one main
 let main = document.querySelector('main');
 let submitButton = document.querySelector('input[type="submit"]');
+// Changed type to button to prevent reloading page
+submitButton.type = 'button';
 
 // Document was misspelled
-let body = document.querySelector('body'); 
+let body = document.querySelector('body');
 let input = document.querySelector('input[type="text"]');
 
 // click is the proper listener, e is never used
-submitButton.addEventListener('click', function() {
-	//input wasn't declared
-  let personName = input.value; 
-  let para = document.createElement('p'); 
-  para.innerHTML = '<h3>Hey there ' + personName + '! Thanks for stopping by!</h3>'; 
-	// paragraph was declared as para above
+submitButton.addEventListener('click', function () {
+  //input wasn't declared
+  let personName = input.value;
+  let para = document.createElement('p');
+  para.innerHTML = '<h3>Hey there ' + personName + '! Thanks for stopping by!</h3>';
+  // paragraph was declared as para above
   main.appendChild(para);
-  body.setAttribute('class', 'meow'); 
-  console.log('=^..^='); 
-}); 
+  body.setAttribute('class', 'meow');
+  console.log('=^..^=');
+});
