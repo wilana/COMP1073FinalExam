58a2d4bcf209568f95e8e1eb52c7222e14a6c148
wilana <wsmatthe@lakeheadu.ca>
2020-04-18 13:23:29 -0400

    Q1

---
+++ Q1/main.js
@@ -0,0 +1,22 @@
+// JavaScript Document
+
+
+// only want one main
+let main = document.querySelector('main');
+let submitButton = document.querySelector('input[type="submit"]');
+
+// Document was misspelled
+let body = document.querySelector('body'); 
+let input = document.querySelector('input[type="text"]');
+
+// click is the proper listener, e is never used
+submitButton.addEventListener('click', function() {
+	//input wasn't declared
+  let personName = input.value; 
+  let para = document.createElement('p'); 
+  para.innerHTML = '<h3>Hey there ' + personName + '! Thanks for stopping by!</h3>'; 
+	// paragraph was declared as para above
+  main.appendChild(para);
+  body.setAttribute('class', 'meow'); 
+  console.log('=^..^='); 
+}); 
